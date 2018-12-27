127a92624d292340040d0a20ae0ae62871e17040
Craig D'Amelio <acdameli@users.noreply.github.com>
2018-12-27 15:37:27 -0800

    Use generator

--- 08-file-searcher/program.py
+++ 08-file-searcher/program.py
@@ -3,16 +3,18 @@ import os
 
 def main():
     print_header('File Search App')
-    string = input('What string are you looking for? ')
+    string = input('What string are you looking for? ').lower()
     directory = input('What directory do you want to search? ')
     print()
     print(f'Searching {directory} for {string}')
     print()
     results = search_directory(directory, string)
+    i = 0
     for l in results:
+        i += 1
         print(l)
     print()
-    print(f'{len(results)} total matches.')
+    print(f'{i} total matches.')
     print(f'End of search')
 
 
@@ -24,21 +26,19 @@ def print_header(string):
 
 
 def search_directory(directory, string):
-    results = []
     for f in os.scandir(directory):
         if f.is_dir():
-            results += search_directory(f, string)
+            yield from search_directory(f, string)
         elif f.is_file():
             try:
-                with open(f, 'r') as fin:
+                with open(f, 'r', encoding='utf-8') as fin:
                     i = 0
                     for line in fin.readlines():
                         i += 1
-                        if line.__contains__(string):
-                            results.append(f'{f.name}, line {i}>> {line.rstrip()}')
+                        if line.lower().find(string) >= 0:
+                            yield f'{f.name}, line {i}>> {line.rstrip()}'
             except UnicodeDecodeError:
                 pass
-    return results
 
 
 if __name__ == '__main__':
