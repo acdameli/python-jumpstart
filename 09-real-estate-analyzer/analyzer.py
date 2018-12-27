23eca0c7ec27436c0197a3844b9ef50a7106f2e4
Craig D'Amelio <acdameli@users.noreply.github.com>
2018-12-27 15:58:16 -0800

    Be a little better

--- 09-real-estate-analyzer/analyzer.py
+++ 09-real-estate-analyzer/analyzer.py
@@ -3,9 +3,41 @@ from collections import defaultdict
 from sys import maxsize
 
 
+class Purchase:
+    def __init__(
+            self, city, zipcode, state, beds,
+            baths, sq__ft, home_type, sale_date, price,
+            latitude, longitude):
+        self.longitude = longitude
+        self.latitude = latitude
+        self.price = price
+        self.sale_date = sale_date
+        self.type = home_type
+        self.sq__ft = sq__ft
+        self.baths = baths
+        self.beds = beds
+        self.state = state
+        self.zip = zipcode
+        self.city = city
+
+    @staticmethod
+    def create_from_dict(lookup):
+        return Purchase(
+            lookup['city'],
+            lookup['zip'],
+            lookup['state'],
+            int(lookup['beds']),
+            int(lookup['baths']),
+            int(lookup['sq__ft']),
+            lookup['type'],
+            lookup['sale_date'],
+            float(lookup['price']),
+            float(lookup['latitude']),
+            float(lookup['longitude']))
+
+
 class Analyzer:
     path = ''
-    info = []
     max = 0
     max_record = None
     min = maxsize
@@ -18,40 +50,39 @@ class Analyzer:
         self.load()
 
     def load(self):
-        # I managed to poop this out in like 10 mins, it works but sucks
-        # todo: make this suck less
-        # 1. use a something better than a dict of strings
-        # 2. drop self.info as soon as analysis is complete... or never use it at all... counter + sums should suffice
+        # do something more elegant for average calculations
         with open(self.path, 'r', encoding='utf-8') as fin:
             reader = DictReader(fin)
             two_bed_count = 0
+            count = 0
             for row in reader:
-                if self.max < int(row['price']):
-                    self.max = max(self.max, int(row['price']))
+                p = Purchase.create_from_dict(row)
+                if self.max < p.price:
+                    self.max = max(self.max, p.price)
                     self.max_record = row
-                if self.min > int(row['price']):
-                    self.min = min(self.min, int(row['price']))
+                if self.min > p.price:
+                    self.min = min(self.min, p.price)
                     self.min_record = row
-                self.averages['price'] += int(row['price'])
-                self.averages['beds'] += int(row['beds'])
-                self.averages['baths'] += int(row['baths'])
-                if int(row['beds']) == 2:
-                    self.two_bed_averages['price'] += int(row['price'])
-                    self.two_bed_averages['beds'] += int(row['beds'])
-                    self.two_bed_averages['baths'] += int(row['baths'])
+                self.averages['price'] += p.price
+                self.averages['beds'] += p.beds
+                self.averages['baths'] += p.baths
+                count += 1
+                if p.beds == 2:
+                    self.two_bed_averages['price'] += p.price
+                    self.two_bed_averages['beds'] += p.beds
+                    self.two_bed_averages['baths'] += p.baths
                     two_bed_count += 1
-                self.info.append(row)
 
-            self.averages['price'] = round(self.averages['price'] / len(self.info))
-            self.averages['beds'] = round(self.averages['beds'] / len(self.info), 1)
-            self.averages['baths'] = round(self.averages['baths'] / len(self.info), 1)
+            self.averages['price'] = round(self.averages['price'] / count)
+            self.averages['beds'] = round(self.averages['beds'] / count, 1)
+            self.averages['baths'] = round(self.averages['baths'] / count, 1)
             if two_bed_count > 0:
                 self.two_bed_averages['price'] = round(self.two_bed_averages['price'] / two_bed_count)
                 self.two_bed_averages['beds'] = round(self.two_bed_averages['beds'] / two_bed_count, 1)
                 self.two_bed_averages['baths'] = round(self.two_bed_averages['baths'] / two_bed_count, 1)
 
     def print_headers(self):
-        print('Header: ' + ', '.join(self.info[0].keys()))
+        print('Header: ' + ', '.join(self.max_record.keys()))
 
     def print_most_expensive(self):
         print('Most expensive: {}-beds, {}-baths, {} for ${:,} in {}'.format(self.max_record['beds'], self.max_record['baths'], self.max_record['type'], int(self.max_record['price']), self.max_record['city']))
